035e3fae7d53f65f435e841726d127a37dcd2b14
tiger <wanhu88@gmail.com>
2015-12-02 17:09:51 +0800

    Initial commit

---
+++ astros-img-interlace.js
@@ -0,0 +1,37 @@
+'use strict';
+
+var path    = require('path'),
+    fs      = require('fs'),
+    images  = require('image-magic');
+
+
+module.exports = new astro.Middleware({
+    fileType: 'img',
+    status  : 'release'
+}, function(asset, next) {
+
+    if(!asset.prjCfg.interlace){
+        next(asset);return;
+    }
+
+    let cache = path.join(asset.prjCfg.imgCache, '_interlace');
+    
+    images.interlace(asset.filePath, cache, function(error){
+
+        if(!error){
+            fs.readFile(cache, function(error, data){
+                if(!error){
+                    asset.data = data;
+                }else{
+                    console.error('astros-img-interlace', 
+                        'readfile error', 'file:', asset.filePath);
+                }
+                next(asset);
+            });
+            return;
+        }
+        next(asset);
+
+    });
+
+});
---
+++ readme.md
@@ -0,0 +1,8 @@
+# astro-img-interlace
+
+发布图片时，如果对图片进行优化，并增加交错属性
+
+### 配置
+在配置文件中加入：
+
+    interlace : true
